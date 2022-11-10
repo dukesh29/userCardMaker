cd7c266713ae6bc7099de1d8f718a3cca1bc6943
Duulat Akmatbekov <duulat.akmatbekov@gmail.com>
2022-11-10 09:06:47 +0600

    Created types.d.ts with all interfaces.
    Added all functions to get FormSubmit on input Change in UserForm.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
+import UserForm from "./components/UserForm/UserForm";
+import {User} from "./types";
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+  const [users, setUsers] = useState<User[]>([
+    {id: '1', name: 'Duulat', role: 'Admin', email: 'duulat.akmatbekov@mail.ru', isActive: true},
+    {id: '2', name: 'Guliza', role: 'Admin', email: 'g.omyrbek@mail.ru', isActive: true}
+  ]);
+
+  const addUser = (newUser: User) => {
+    setUsers(prevState => [...prevState, newUser]);
+  };
+
+
+  return (
+    <>
+      <main className="container-fluid">
+        <div className="row mt-2">
+          <div className="col">
+            <UserForm onSubmit={addUser}></UserForm>
+          </div>
+          <div className="col">
+
+          </div>
+        </div>
+      </main>
+    </>
   );
 }
 
--- src/components/UserForm/UserForm.tsx
+++ src/components/UserForm/UserForm.tsx
@@ -1,10 +1,90 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {User, UserMutation} from "../../types";
 
-const UserForm = () => {
+interface Props {
+  onSubmit: (user: User) => void;
+}
+
+const UserForm: React.FC<Props> = ({onSubmit}) => {
+
+  const [user, setUser] = useState<UserMutation>({
+    name: '',
+    email: '',
+    isActive: false,
+    role: '',
+  });
+
+  const onTextTyping = (e: React.ChangeEvent<HTMLInputElement | HTMLSelectElement>) => {
+    const {name, value} = e.target;
+    setUser(prev => ({...prev, [name]: value}));
+  };
+
+  const onCheck = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const {name, checked} = e.target;
+    setUser(prev => ({...prev, [name]: checked}));
+  };
+
+
+  const onFormSubmit =(e:React.FormEvent) => {
+    e.preventDefault();
+    onSubmit ({
+      id:Math.random().toString(),
+      ...user,
+    })
+  };
+
   return (
-    <>
 
-    </>
+    <form onSubmit={onFormSubmit}>
+      <h4>Add new user</h4>
+      <div className="form-group">
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          className="form-control"
+          value={user.name}
+          onChange={onTextTyping}
+        />
+      </div>
+      <div className="form-group">
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          name="email"
+          type="email"
+          required
+          className="form-control"
+          value={user.email}
+          onChange={onTextTyping}
+        />
+      </div>
+      <div className="form-group my-3">
+        <label>Active <input
+          id="active"
+          name="isActive"
+          type="checkbox"
+          checked={user.isActive}
+          onChange={onCheck}
+        /> </label>
+      </div>
+      <div className="form-group mb-4">
+        <label htmlFor="role">Role</label>
+        <select
+          name="role"
+          required
+          value={user.role}
+          onChange={onTextTyping}
+        >
+          <option disabled value=''>Select your role</option>
+          <option>User</option>
+          <option>Editor</option>
+          <option>Admin</option>
+        </select>
+      </div>
+      <button type="submit" className="btn btn-primary">Create</button>
+    </form>
   );
 };
 
